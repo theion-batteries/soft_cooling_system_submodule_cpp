7fc4554d7530a4167af16fc79880771c8dade127
sami-theion <sami@theion.de>
2023-01-12 14:26:55 +0100

    updated cmake

--- includes/ph_cooling_controller.h
+++ includes/ph_cooling_controller.h
@@ -29,7 +29,7 @@
 #ifdef SINK_SENSOR_MOCK
 #include "sensorMock.h"
 #else
-//#include "keyence_sensor.h"
+#include "meteorAdapter.h"
 #endif
 #ifdef SINK_AXIS_MOCK
 #include "axisMock.h"
@@ -37,21 +37,9 @@
 #include "linear_motion.h"
 #endif
 
-struct whs_config_yaml_params
+struct cooling_config_yaml_params
 {
-    LPCTSTR pyInterpreter;
-    LPSTR pyFile;
-    LPSTR pyCmd;
-    std::string py;
-    std::string fi;
-    std::string cmd;
-    double mm_steps; // distance to move down  default 10 mm
-    DWORD delay_to_move_request; // wait between move request  default 2000ms
-    double ref_dis;// parameter calibration sensor  default 
-    double thickness; // thickness of wafer holder default 0.1mm
-    double mm_step_res;
-    int one_mm_steps = 1;
-    int MaxSafePos = 250;
+  
 
 };
 
@@ -59,32 +47,32 @@ class ph_cooling_controller
 {
 public:
      YAML::Node config;
-     whs_config_yaml_params _whs_params;
+     cooling_config_yaml_params _whs_params;
 private:
-    bool waferHolderReady = false;
-    std::shared_ptr< Idistance_sensor> distSensor;
+    bool phCoolingControllerReady = false;
+    std::shared_ptr< meteorAdapter> ph;
     std::shared_ptr <Iaxis_motion> linearMover;
     double current_axis_position;
-    double current_sensor_value;
 public:
     /******* const/desctr ****/
     ph_cooling_controller();
     ~ph_cooling_controller();
     /******** algorithms controller **********/
 
-    void move_down_until_data_availble();
-    void move_down_to_surface();
-    void deep_wafer_holder_desired_thickness(); //default to 0.01 mm_step x 10 steps= 0.1mm or 100µm
-    void monitor_and_calibrate(); // we will launch a monitor to check if we are inside the desired depth during next processes
+
+
+
+
+
+
     /********* helper functions */
     bool get_mover_status();
-    bool get_sensor_status();
+    bool get_ph_status();
     bool get_ph_cooling_controller_status();
     /*     helper getter */
-    double get_sensor_values();
     double get_axis_position();
     Iaxis_motion* get_axis_ptr();
-    Idistance_sensor* get_dist_ptr();
+    meteorAdapter* get_ph_ptr();
     void sendDirectCmdSensor(std::string& cmd);
     std::string sendDirectCmdAxis(std::string cmd);
      void reload_config_file();
--- src/ph_cooling_controller.cpp
+++ src/ph_cooling_controller.cpp
@@ -10,38 +10,32 @@
  */
 
 #include "ph_cooling_controller.h"
-#include <fstream>
 
 
 ph_cooling_controller::ph_cooling_controller()
 {
     std::cout << "creating subsystem wafer holder motion controller " << std::endl;
-#ifdef WHS_CONFIG
-    std::cout << "loading config file: "<< WHS_CONFIG<< std::endl;
-    std::ifstream filein(WHS_CONFIG);
+#ifdef PH_CONFIG
+    std::cout << "loading config file: "<< PH_CONFIG<< std::endl;
+    std::ifstream filein(PH_CONFIG);
     for (std::string line; std::getline(filein, line); )
     {
         std::cout << line << std::endl;
     }
-    config = YAML::LoadFile(WHS_CONFIG);
-    _whs_params.fi = (config["script"].as<std::string>());
-    _whs_params.py = (config["interpreter"].as<std::string>());
-    _whs_params.cmd = _whs_params.py + " " + _whs_params.fi;
-    _whs_params.pyCmd = &_whs_params.cmd[0];
-    _whs_params.pyFile = &_whs_params.fi[0];
-    _whs_params.pyInterpreter = _whs_params.py.c_str();
-    _whs_params.mm_steps = config["mm_steps"].as<double>();
-    _whs_params.mm_step_res = config["mm_step_res"].as<double>();
-    _whs_params.ref_dis = config["ref_dis"].as<double>();
-    _whs_params.delay_to_move_request = config["delay_to_move_request"].as<DWORD>();
-    _whs_params.thickness = config["thickness"].as<double>();
-    _whs_params.MaxSafePos = config["MaxSafePos"].as<double>();
-
+    config = YAML::LoadFile(PH_CONFIG);
+//
+//    _whs_params.mm_steps = config["mm_steps"].as<double>();
+//    _whs_params.mm_step_res = config["mm_step_res"].as<double>();
+//    _whs_params.ref_dis = config["ref_dis"].as<double>();
+//    _whs_params.delay_to_move_request = config["delay_to_move_request"].as<DWORD>();
+//    _whs_params.thickness = config["thickness"].as<double>();
+//    _whs_params.MaxSafePos = config["MaxSafePos"].as<double>();
+//
 #endif 
 #ifdef SINK_SENSOR_MOCK
-    distSensor = std::make_shared< sensorMock>();
+    ph = std::make_shared< sensorMock>();
 #else
-    distSensor = std::make_shared< keyence_sensor>();
+    ph = std::make_shared< meteorAdapter>();
 #endif
 #ifdef SINK_AXIS_MOCK
     linearMover = std::make_shared< axisMock>();
@@ -59,150 +53,24 @@ ph_cooling_controller::~ph_cooling_controller()
 
 void ph_cooling_controller::reload_config_file()
 {
-    std::cout << "reloading config file: " << WHS_CONFIG<<std::endl;
-    std::ifstream filein(WHS_CONFIG);
+    std::cout << "reloading config file: " << PH_CONFIG<<std::endl;
+    std::ifstream filein(PH_CONFIG);
     for (std::string line; std::getline(filein, line); )
     {
         std::cout << line << std::endl;
     }
-    config = YAML::LoadFile(WHS_CONFIG);
-    _whs_params.fi = (config["script"].as<std::string>());
-    _whs_params.py = (config["interpreter"].as<std::string>());
-    _whs_params.cmd = _whs_params.py + " " + _whs_params.fi;
-    _whs_params.pyCmd = &_whs_params.cmd[0];
-    _whs_params.pyFile = &_whs_params.fi[0];
-    _whs_params.pyInterpreter = _whs_params.py.c_str();
-    _whs_params.mm_steps = config["mm_steps"].as<double>();
-    _whs_params.mm_step_res = config["mm_step_res"].as<double>();
-    _whs_params.ref_dis = config["ref_dis"].as<double>();
-    _whs_params.delay_to_move_request = config["delay_to_move_request"].as<DWORD>();
-    _whs_params.thickness = config["thickness"].as<double>();
-    _whs_params.MaxSafePos = config["MaxSafePos"].as<double>();
+    config = YAML::LoadFile(PH_CONFIG);
+    //_whs_params.mm_steps = config["mm_steps"].as<double>();
+    //_whs_params.mm_step_res = config["mm_step_res"].as<double>();
+    //_whs_params.ref_dis = config["ref_dis"].as<double>();
+    //_whs_params.delay_to_move_request = config["delay_to_move_request"].as<DWORD>();
+    //_whs_params.thickness = config["thickness"].as<double>();
+    //_whs_params.MaxSafePos = config["MaxSafePos"].as<double>();
 
 }
 /**************** Algorithms conntroller ***************/
 
 
-void ph_cooling_controller::move_down_until_data_availble()
-{
-    linearMover->move_home();
-    current_axis_position = linearMover->get_position();
-    
-    std::cout << "axis start pos:  " << current_axis_position << std::endl;
-
-    while (distSensor->getMesuredValue() == 0 ) // while data invalid, we go down further
-    {
-        // algo1: given max trajectory 
-        if (_whs_params.MaxSafePos>_whs_params.mm_steps)
-        {
-        std::cout << "moving all way down to " <<_whs_params.MaxSafePos << "mm_steps until reading values " << std::endl;
-        linearMover->move_down_to(-_whs_params.MaxSafePos); // move down to max
-        }
-        // algo2: move long steps
-        else
-        {
-        std::cout << "moving down by " << _whs_params.mm_steps << "mm_steps until reading values " << std::endl;
-        linearMover->move_down_to(current_axis_position- _whs_params.mm_steps); // move down by mm steps
-        current_axis_position=current_axis_position- _whs_params.mm_steps;
-        std::cout << "new axis position "<< current_axis_position   << std::endl;  
-        }
-          
-    }
-    std::cout << "sensor value: " << distSensor->getMesuredValue() << std::endl;
-    std::cout << "sensor data valid" << std::endl;
-    std::cout << "algorithm finished succeffuly " << std::endl;
-    std::cout << "<----------------------------------------------> " << std::endl;
-
-
-
-}
-/**
- * @brief
- *
- * @param ref_dis
- */
-void ph_cooling_controller::move_down_to_surface()
-{
-    std::cout << "<----------------------------------------------> " << std::endl;
-        std::cout << "algorithm surface contact launched" << std::endl;
-
-    std::cout << "moving down to surface" << std::endl;
-    current_axis_position = linearMover->get_position();
-    std::cout << "current pos:  " <<  current_axis_position<< std::endl;
-
-    while (distSensor->getMesuredValue() >= _whs_params.ref_dis) // while distSensor reading is <= to reference distance
-    {
-        std::cout << "moving down until sensor reading is equal the refernce distance: " << _whs_params.ref_dis << std::endl;
-        std::cout << "moving down by " << _whs_params.one_mm_steps <<  std::endl;
-        linearMover->move_down_to(current_axis_position-_whs_params.one_mm_steps); // move down by mm steps
-        current_axis_position=current_axis_position- _whs_params.one_mm_steps;
-        std::cout << "new axis position "<< current_axis_position<< std::endl;  
-    }
-    std::cout << "sensor value: " << distSensor->getMesuredValue() << std::endl;
-    std::cout << "algorithm finished succeffuly " << std::endl;
-    std::cout << "<----------------------------------------------> " << std::endl;
-}
-/*
- * @brief
- *
- * @param thickness
- * @param mm_step_res
- */
-void ph_cooling_controller::deep_wafer_holder_desired_thickness() //default to 0.01 mm_step x 10 steps= 0.1mm or 100µm
-{
-    std::cout << "algorithm deeping launched " << std::endl;
-    std::cout << "<----------------------------------------------> " << std::endl;
-    auto steps = _whs_params.thickness / _whs_params.mm_step_res;
-    current_axis_position = get_axis_position();
-    for (unsigned int step_counter = 0; step_counter < steps; step_counter++)
-    {
-        std::cout << "iteration number " << step_counter << std::endl;
-        linearMover->move_down_to(current_axis_position- _whs_params.mm_step_res); // move mm step default 0.01 mm
-        current_axis_position -= _whs_params.mm_step_res;
-        distSensor->getMesuredValue();
-        linearMover->get_position();
-    }
-        distSensor->getMesuredValue();
-        linearMover->get_position();
-    waferHolderReady = true;
-    std::cout << "algorithm finished succeffuly " << std::endl;
-    std::cout << "<----------------------------------------------> " << std::endl;
-}
-/**
- * @brief function to monitor the distance and calibrate depth
- */
-void ph_cooling_controller::monitor_and_calibrate()
-{
-    std::cout << "monitor calibration started" << std::endl;
-    // algorithm: if current distance is not equal desired thickness, calibrate
-    // 1. check if diff is a value that is below the step resolution, continue or
-    // 2. move up if diff negativ
-    // 3. move down if diff positiv
-    while (true)
-    {
-        double diff = distSensor->getMesuredValue() - _whs_params.thickness;
-        if (abs(diff) <= _whs_params.mm_step_res) // if the difference mesured lower than min step res, we skip calibration
-        {
-            continue;
-        }
-        else
-        {
-            if (diff < 0) // if diff negativ, we move up
-            {
-                linearMover->move_up_by(diff);
-            }
-            else
-            {
-                linearMover->move_down_by(diff);
-
-            }
-
-        }
-
-    }
-
-
-}
 
 
 /********* helper functions */
@@ -210,22 +78,18 @@ bool ph_cooling_controller::get_mover_status()
 {
     return linearMover->getStatus();
 }
-bool ph_cooling_controller::get_sensor_status()
+bool ph_cooling_controller::get_ph_status()
 {
-    return distSensor->getStatus();
+    return ph->getStatus();
 }
 bool ph_cooling_controller::get_ph_cooling_controller_status()
 {
-    return waferHolderReady;
+    return phCoolingControllerReady;
 }
 
 /*                getter interface              */
 
-double ph_cooling_controller::get_sensor_values()
-{
 
-    return distSensor->getMesuredValue();
-}
 double ph_cooling_controller::get_axis_position()
 {
 
@@ -236,9 +100,9 @@ Iaxis_motion* ph_cooling_controller::get_axis_ptr()
 {
     return dynamic_cast<Iaxis_motion*>(linearMover.get());
 }
-Idistance_sensor* ph_cooling_controller::get_dist_ptr()
+meteorAdapter* ph_cooling_controller::get_ph_ptr()
 {
-    return dynamic_cast<Idistance_sensor*>(distSensor.get());
+    return dynamic_cast<meteorAdapter*>(ph.get());
 }
 /**
  * @brief TODO implement direct send cmd to sensor if needed
@@ -247,7 +111,7 @@ Idistance_sensor* ph_cooling_controller::get_dist_ptr()
  */
 void ph_cooling_controller::sendDirectCmdSensor(std::string& cmd)
 {
-   // distSensor->sendDirectCmd(cmd);
+   // ph->sendDirectCmd(cmd);
 }
 std::string ph_cooling_controller::sendDirectCmdAxis(std::string cmd)
 {
