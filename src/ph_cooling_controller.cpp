06752f4ffcf6246cc7666616353f2907f7b375de
saravanan-theion <saravanan@theion.de>
2023-03-13 16:52:50 +0100

    changed std::endl to \n

--- src/ph_cooling_controller.cpp
+++ src/ph_cooling_controller.cpp
@@ -13,16 +13,15 @@
 using enum wgm_feedbacks::enum_hw_feedback;
 using enum wgm_feedbacks::enum_sub_sys_feedback;
 
-
 ph_cooling_controller::ph_cooling_controller()
 {
-    std::cout << "creating subsystem cooling ph controller " << std::endl;
+    std::cout << "creating subsystem cooling ph controller " << "\n";
 #ifdef PH_CONFIG
-    std::cout << "loading config file: " << PH_CONFIG << std::endl;
+    std::cout << "loading config file: " << PH_CONFIG << "\n";
     std::ifstream filein(PH_CONFIG);
-    for (std::string line; std::getline(filein, line); )
+    for (std::string line; std::getline(filein, line);)
     {
-        std::cout << line << std::endl;
+        std::cout << line << "\n";
     }
     config = YAML::LoadFile(PH_CONFIG);
     _ph_params.ph_rotate_to_center = config["ph_rotate_to_center"].as<double>();
@@ -45,7 +44,6 @@ ph_cooling_controller::ph_cooling_controller()
     _ph_params.ph_trigger_server_ip = config["ph_trigger_server_ip"].as<std::string>();
     _ph_params.ph_trigger_server_port = config["ph_trigger_server_port"].as<uint16_t>();
 
-
     // motion /rotation
     _ph_params.phead_travel = config["phead_travel"].as<double>();
     _ph_params.phead_max_travel = config["phead_max_travel"].as<double>();
@@ -57,17 +55,13 @@ ph_cooling_controller::ph_cooling_controller()
     _ph_params.phead_start_angle = config["phead_start_angle"].as<double>();
     _ph_params.timeout = config["timeout"].as<uint16_t>();
 
-#endif 
+#endif
 
+    ph = std::make_shared<meteorAdapter>(_ph_params);
 
-    ph = std::make_shared< meteorAdapter>(_ph_params);
+    motionMover = std::make_shared<ph_xy_motion>(_ph_params.ph_motion_server_ip, _ph_params.ph_motion_server_port, _ph_params.timeout);
 
-
-    motionMover = std::make_shared< ph_xy_motion>(_ph_params.ph_motion_server_ip, _ph_params.ph_motion_server_port,_ph_params.timeout);
-
-
-    phTrigger = std::make_shared< ph_trigger>(_ph_params.ph_trigger_server_ip, _ph_params.ph_trigger_server_port, _ph_params.timeout);
-
+    phTrigger = std::make_shared<ph_trigger>(_ph_params.ph_trigger_server_ip, _ph_params.ph_trigger_server_port, _ph_params.timeout);
 }
 /**
  * @brief Destroy the whs controller::whs controller object
@@ -81,74 +75,93 @@ ph_cooling_controller::~ph_cooling_controller()
 
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_connect_engine()
 {
-    if (ph->connect() == hw_error) return sub_error;
+    if (ph->connect() == hw_error)
+        return sub_error;
     return sub_success;
 }
 
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_controller_connect()
 {
-    if (motionMover->connect() == sub_error || phTrigger->connect() == sub_error || ph->connect() == hw_error) return sub_error;
+    if (motionMover->connect() == sub_error || phTrigger->connect() == sub_error /*|| ph->connect() == hw_error*/)
+        return sub_error;
     phCoolingControllerReady = true;
     return sub_success;
 }
+
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_controller_disconnect()
 {
-    if (motionMover->disconnect() == sub_error || phTrigger->disconnect() == sub_error || ph->disconnect() == hw_error) return sub_error;
-    phCoolingControllerReady = true;
+    if (motionMover->disconnect() == sub_error || phTrigger->disconnect() == sub_error || ph->disconnect() == hw_error)
+        return sub_error;
+    phCoolingControllerReady = false;
     return sub_success;
 }
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_motion_home_all()
 {
-    if (motionMover->home_all() == sub_error) return sub_error;
+    if (motionMover->home_all() == sub_error)
+        return sub_error;
     return sub_success;
 }
+
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_motion_move_home()
 {
-    if (motionMover->move_home() == sub_error) return sub_error;
+    if (motionMover->move_home() == sub_error)
+        return sub_error;
     return sub_success;
 }
-wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_motion_move_offset()
+
+/*wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_motion_move_offset()
 {
-    if (motionMover->move_down_to(50) == sub_error) return sub_error;
+    double offset_pos = 50; // offset position in mm
+    if (motionMover->move_down_to(offset_pos) == sub_error)
+        return sub_error;
+    return sub_success;
+}
+*/
+wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_motion_move_offset(const double offset)
+{
+    if (motionMover->move_down_to(offset) == sub_error)
+        return sub_error;
     return sub_success;
 }
 
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_motion_rotate_home()
 {
-    if (motionMover->rotate_home() == sub_error) return sub_error;
+    if (motionMover->rotate_home() == sub_error)
+        return sub_error;
     return sub_success;
 }
-wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_motion_move_to_center(double new_pos)
-{
-    return    motionMover->move_down_to(new_pos);
 
+wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_motion_move_to_center(const double new_pos)
+{
+    return motionMover->move_down_to(new_pos);
 }
-wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_rotate_to_center(double new_pos)
+wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_rotate_to_center(const double degree)
 {
-    return    motionMover->rotate_to(new_pos);
+    return motionMover->rotate_to(degree);
+}
 
+wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_rotate_to(const double degree)
+{
+    return motionMover->rotate_to(degree);
 }
 
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_move_center()
 {
-    return    motionMover->move_down_to(_ph_params.distance_to_center);
-
+    return motionMover->move_down_to(_ph_params.distance_to_center);
 }
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_rotate_center()
 {
-    return    motionMover->rotate_to(_ph_params.ph_rotate_to_center);
-
+    return motionMover->rotate_to(_ph_params.ph_rotate_to_center);
 }
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_trigger_print()
 {
-    return    phTrigger->turn_on();
-
+    return phTrigger->turn_on();
 }
 
 wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_rotate_and_print()
 {
 
-    /*
+    /* TODO
     // rotate thread
     // print thread
     // Algoritm:
@@ -157,8 +170,41 @@ wgm_feedbacks::enum_sub_sys_feedback ph_cooling_controller::ph_rotate_and_print(
     3. after 5 rev: stop printing and rotation
     */
 
-    phTrigger->turn_on();
-    return sub_error;
+    const double one_rotation = 360; // degrees to finish to one rotation
+    const double complete_roation = _ph_params.phead_rotations * one_rotation;
+    const double home_rotation = -90;
+    ph_xy_motion::setModeBlocking(true);
+    auto result = motionMover->rotate_to(one_rotation); // complete one rotation
+    if (result == sub_error)
+     {
+        std::cout<<" Failed to rotate. exiting . \n";
+        return sub_error;
+     }
+
+    result = phTrigger->turn_on();
+    if (result == sub_error)
+     {
+        std::cout<<" Failed to trigger. exiting . \n";
+        return sub_error;
+     }
+
+    ph_xy_motion::setModeBlocking(true);
+    result = motionMover->rotate_to(complete_roation);
+    if (result == sub_error)
+     {
+        std::cout<<" Failed to rotate complete number of turns. exiting. \n";
+        return sub_error;
+     }
+
+/*    ph_xy_motion::setModeBlocking(true);
+    result = motionMover->rotate_to(home_rotation);
+    if (result == sub_error)
+     {
+        std::cout<<" Failed to rotate to home position. \n";
+        return sub_error;
+     } */
+
+    return sub_success;
 }
 
 
@@ -195,62 +241,65 @@ bool ph_cooling_controller::get_ph_cooling_controller_status()
 
 /*                getter interface              */
 
-
 double ph_cooling_controller::get_axis_position()
 {
 
     return motionMover->get_linear_position();
 }
+
 double ph_cooling_controller::get_rotation_position()
 {
 
     return motionMover->get_rotation_position();
 }
+
 double ph_cooling_controller::get_axis_velocity()
 {
 
     return motionMover->get_linear_speed();
 }
+
 double ph_cooling_controller::get_rotation_velocity()
 {
 
     return motionMover->get_rotation_speed();
 }
 
-Iph_xy_motion* ph_cooling_controller::get_xy_axis_ptr()
+Iph_xy_motion *ph_cooling_controller::get_xy_axis_ptr()
 {
-    return dynamic_cast<Iph_xy_motion*>(motionMover.get());
+    return dynamic_cast<Iph_xy_motion *>(motionMover.get());
 }
-ph_trigger* ph_cooling_controller::get_trigger_ptr()
+ph_trigger *ph_cooling_controller::get_trigger_ptr()
 {
-    return dynamic_cast<ph_trigger*>(phTrigger.get());
+    return dynamic_cast<ph_trigger *>(phTrigger.get());
 }
-meteorAdapter* ph_cooling_controller::get_ph_ptr()
+meteorAdapter *ph_cooling_controller::get_ph_ptr()
 {
-    return dynamic_cast<meteorAdapter*>(ph.get());
+    return dynamic_cast<meteorAdapter *>(ph.get());
 }
 /**
  * @brief TODO implement direct send cmd to sensor if needed
  *
  * @param cmd
  */
-void ph_cooling_controller::sendDirectCmdSensor(std::string& cmd)
+void ph_cooling_controller::sendDirectCmdSensor(std::string &cmd)
 {
     // ph->sendDirectCmd(cmd);
 }
+
 std::string ph_cooling_controller::sendDirectCmdAxis(std::string cmd)
 {
-    return  motionMover->sendDirectCmd(cmd);
+    return motionMover->sendDirectCmd(cmd);
 }
 
 void ph_cooling_controller::reload_config_file()
 {
 
-    std::cout << "reloading config file: " << PH_CONFIG << std::endl;
+    std::cout << "reloading config file: " << PH_CONFIG << "\n";
     std::ifstream filein(PH_CONFIG);
-    for (std::string line; std::getline(filein, line); )
+    for (std::string line; std::getline(filein, line);)
     {
-        std::cout << line << std::endl;
+        std::cout << line << "\n";
     }
     config = YAML::LoadFile(PH_CONFIG);
     _ph_params.ph_rotate_to_center = config["ph_rotate_to_center"].as<double>();
@@ -283,5 +332,4 @@ void ph_cooling_controller::reload_config_file()
     _ph_params.phead_intermediate_stop = config["phead_intermediate_stop"].as<double>();
     _ph_params.phead_start_angle = config["phead_start_angle"].as<double>();
     _ph_params.timeout = config["timeout"].as<uint16_t>();
-
 }
--- src/ph_trigger.cpp
+++ src/ph_trigger.cpp
@@ -5,118 +5,125 @@ ph_trigger::ph_trigger(std::string ip, uint16_t port, const uint16_t timeout)
     _trigger_struct.ip = ip;
     _trigger_struct.port = port;
     _trigger_struct.timeout = timeout;
-
 }
 
 std::string ph_trigger::sendDirectCmd(std::string cmd)
 {
-    if (_client == nullptr) return "not connected";
-    std::cout << "sending command " << cmd << std::endl;
+    if (_client == nullptr)
+        return "not connected";
+    std::cout << "sending command " << cmd << "\n";
     cmd = cmd + "\r\n";
 
-    if (_client->write(cmd) != ssize_t(std::string(cmd).length())) {
+    if (_client->write(cmd) != ssize_t(std::string(cmd).length()))
+    {
         std::cout << "Error writing to the TCP stream: "
-            << _client->last_error_str() << std::endl;
+                  << _client->last_error_str() << "\n";
     }
     return waitForResponse();
 }
 std::string ph_trigger::waitForResponse()
 {
-     std::cout << "awaiting server response" << std::endl;
+    // std::cout << "awaiting server response" << "\n";
     auto start = std::chrono::steady_clock::now();
     while (_client->is_connected())
     {
 
         char Strholder[5012];
-        
-            ssize_t n = _client->read_n(&Strholder, sizeof(Strholder));
+
+        ssize_t n = _client->read_n(&Strholder, sizeof(Strholder));
         if (n > 0)
         {
-            std::cout << "n bytes received: " << n << std::endl;
+            std::cout << "n bytes received: " << n << "\n";
             incoming_data = Strholder;
             incoming_data.resize(n);
-            std::cout << "server replied : " << incoming_data << std::endl;
+            std::cout << "server replied : " << incoming_data << "\n";
             break;
         }
         else
         {
-            std::cout << "no server response, retry " << n << std::endl;
+            //            std::cout << "no server response, retry " << n << "\n";
             incoming_data = "NA";
-            long long timeout = 10;
+            long long timeout = _trigger_struct.timeout;
             auto duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - start).count();
-            if(duration >= timeout)
+            if (duration >= timeout)
             {
-            std::cout << "no response within a timeout of "<<duration<< " seconds, " <<"aborting.."<< std::endl;
-            break;
-            } 
+                std::cout << "no response within a timeout of " << duration << " seconds, "
+                          << "aborting.." << "\n";
+                break;
+            }
             continue;
         }
-
     }
+    _client->set_non_blocking(true);
+
     return incoming_data;
 }
 wgm_feedbacks::enum_sub_sys_feedback ph_trigger::connect()
 {
-    std::cout << "connecting controller to trigger server" << std::endl;
-    std::cout << "trigger server ip:  " << _trigger_struct.ip << std::endl;
-    _client = new sockpp::tcp_connector({ _trigger_struct.ip, _trigger_struct.port });
+    std::cout << "connecting controller to trigger server" << "\n";
+    std::cout << "trigger server ip:  " << _trigger_struct.ip << "\n";
+    _client = new sockpp::tcp_connector({_trigger_struct.ip, _trigger_struct.port});
     _client->set_non_blocking();
     // Implicitly creates an inet_address from {host,port}
     // and then tries the connection.
-    if (!_client->is_connected()) {
+    if (!_client->is_connected())
+    {
         std::cerr << "Error connecting to trigger server at "
-            << sockpp::inet_address(_trigger_struct.ip, _trigger_struct.port)
-            << " -> " << _client->last_error_str();
+                  << sockpp::inet_address(_trigger_struct.ip, _trigger_struct.port)
+                  << " -> " << _client->last_error_str();
         triggerReady = false;
         return wgm_feedbacks::enum_sub_sys_feedback::sub_error;
     }
-    std::cout << "Created a connection from " << _client->address() << std::endl;
-    std::cout << "Created a connection to " << _client->peer_address() << std::endl;
+    std::cout << "Created a connection from " << _client->address() << "\n";
+    std::cout << "Created a connection to " << _client->peer_address() << "\n";
     // Set a timeout for the responses
-    if (!_client->read_timeout(std::chrono::seconds(5))) {
+    if (!_client->read_timeout(std::chrono::seconds(5)))
+    {
         std::cerr << "Error setting timeout on TCP stream: "
-            << _client->last_error_str() << std::endl;
+                  << _client->last_error_str() << "\n";
         triggerReady = false;
         return wgm_feedbacks::enum_sub_sys_feedback::sub_error;
     }
     triggerReady = true;
     return wgm_feedbacks::enum_sub_sys_feedback::sub_success;
-
 }
 wgm_feedbacks::enum_sub_sys_feedback ph_trigger::disconnect()
 {
     triggerReady = false;
-    if (_client->close()) return sub_success;
+    if (_client->close())
+        return sub_success;
     return sub_error;
-
 }
 wgm_feedbacks::enum_sub_sys_feedback ph_trigger::turn_on()
 {
+    _client->set_non_blocking(false);
     auto command = trigger_cmds.find("turn_on");
-    if (command != trigger_cmds.end()) {
+    if (command != trigger_cmds.end())
+    {
         std::cout << "sending command: " << command->second << '\n';
         auto reply = sendDirectCmd(command->second);
         std::cout << " reply received " << reply << '\n';
-        if (reply == "ok") return sub_success;
+        if (reply.find("ok")!=std::string::npos)
+            return sub_success;
         return sub_error;
     }
     return sub_error;
-
 }
 wgm_feedbacks::enum_sub_sys_feedback ph_trigger::turn_off()
 {
-    auto command = trigger_cmds.find("turn_on");
-    if (command != trigger_cmds.end()) {
+    _client->set_non_blocking(false);
+    auto command = trigger_cmds.find("turn_off");
+    if (command != trigger_cmds.end())
+    {
         std::cout << "sending command: " << command->second << '\n';
         auto reply = sendDirectCmd(command->second);
         std::cout << " reply received " << reply << '\n';
-        if (reply == "ok") return sub_success;
+        if (reply.find("ok")!=std::string::npos)
+            return sub_success;
         return sub_error;
     }
     return sub_error;
-
 }
-
 
 bool ph_trigger::getStatus()
 {
